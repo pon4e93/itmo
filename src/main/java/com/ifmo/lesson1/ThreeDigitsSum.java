fab1b1e769fc01a073b23ab5278a3af758906753
pavel <pon4e93@gmail.com>
2019-10-17 20:45:11 +0300

    Init

---
+++ src/main/java/com/ifmo/lesson1/ThreeDigitsSum.java
@@ -0,0 +1,21 @@
+package com.ifmo.lesson1;
+
+public class ThreeDigitsSum {
+    /*
+    В переменной n хранится натуральное трёхзначное число.
+    Создайте программу, вычисляющую и выводящую на экран сумму цифр числа n.
+     */
+    public static void main(String[] args) {
+        int n = 123;
+
+        int sum = sum(n);
+
+        System.out.println(sum);
+    }
+
+    public static int sum(int n) {
+        // TODO implement
+
+        return 0;
+    }
+}
---
+++ src/main/java/com/ifmo/lesson1/TwoDigitsSum.java
@@ -0,0 +1,21 @@
+package com.ifmo.lesson1;
+
+public class TwoDigitsSum {
+    /*
+    В переменной n хранится натуральное двузначное число.
+    Создайте программу, вычисляющую и выводящую на экран сумму цифр числа n.
+     */
+    public static void main(String[] args) {
+        int n = 23;
+
+        int sum = sum(n);
+
+        System.out.println(sum);
+    }
+
+    public static int sum(int n) {
+        // TODO implement
+
+        return 0;
+    }
+}
